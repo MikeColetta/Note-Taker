cca224417b5eb7cb3744d057f6f0986c2e4f26c3
Mike Coletta <coletta.mike@gmail.com>
2021-03-08 16:48:18 -0800

    Added Delete functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,18 +7,29 @@ module.exports = (app) => {
     app.post('/api/notes', (req, res) => {
         storedNotes.push(req.body);
         try {
-            console.log(storedNotes)
             fs.writeFileSync('./db/db.json', JSON.stringify(storedNotes, null, 4))
             res.json(true)
         }
         catch (error) {
-            console.log(error)
             res.json(error)
         }
      });
 
     app.delete('/api/notes/:id', (req, res) => {
-        console.log(req.params.id)
+        let indexToDelete = -1
+        storedNotes.forEach((note, index) => {
+            if (note.id === req.params.id) {
+                indexToDelete = index
+            }
+        })
+        storedNotes.splice(indexToDelete, 1)
+        try {
+            fs.writeFileSync('./db/db.json', JSON.stringify(storedNotes, null, 4))
+            res.json(true)
+        }
+        catch (error) {
+            res.json(error)
+        }
     });
 
 }
